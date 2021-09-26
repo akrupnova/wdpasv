7f9a808f0f0b2b3f09f53649070f7cf3654f5418
Anna <anna.krupnovaa@gmail.com>
2021-09-26 00:22:31 -0700

    crossbrowser

---
+++ config/common/hooks.conf.js
@@ -0,0 +1,15 @@
+import {addCommands} from '../../helpers/setup.helper';
+import allure from '@wdio/allure-reporter';
+
+module.exports = {
+    before: async function (capabilities, specs, browser) {
+        await addCommands();
+    },
+
+    afterTest: async function (test, context, result) {
+        if (test.failed || result.error) {
+            await browser.takeScreenshot();
+            allure.addAttachment('URL', await browser.getUrl(), 'text/plain');
+        }
+    }
+};
