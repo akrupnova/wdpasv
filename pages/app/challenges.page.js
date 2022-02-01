2b0b165f2432cf356313774c87a0d625956ed84b
akrupnova <75409569+akrupnova@users.noreply.github.com>
2022-02-01 02:11:31 -0800

    update (#7)

--- pages/app/challenges.page.js
+++ pages/app/challenges.page.js
@@ -1,8 +1,10 @@
 import BasePage from './base.page';
 
 class ChallengesPage extends BasePage {
-   // get headerItem() { return $('//h1[contains(@class, "jumbo-title")][contains(text(), "challenges")]') }
-    get headerItem() { return $('//div[text()=\'Challenges\']') }
+  // get headerItem() { return $('//h1[contains(@class, "jumbo-title")][contains(text(), "challenges")]') }
+   // get headerItem() { return $('//div[text()="Challenges"]') }
+ //  get headerItem() { return $('//div[@class="h1 me-4"]') }
+   get headerItem() { return $('//div[@class="h1 me-4"]') }
 
 }
 
--- pages/app/diary.page.js
+++ pages/app/diary.page.js
@@ -1,6 +1,7 @@
 import BasePage from './base.page';
 
 class DiaryPage extends BasePage {
+    //  get headerItem() { return $('//h1[contains(@class, "jumbo-title")][contains(text(), "Diaries")]') }
     get headerItem() { return $('//h1[text()="Daily reports"]') }
 }
 
